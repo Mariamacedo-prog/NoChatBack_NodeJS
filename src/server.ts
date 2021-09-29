07a618994f37e75a275d6cc46e368e6eb583c8a9
unknown <mariamacedo2xx@gmail.com>
2021-09-29 14:58:27 -0300

    Configurando o multer para upload de arquivo junto ao AWS s3

--- src/server.ts
+++ src/server.ts
@@ -40,5 +40,5 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
 server.use(errorHandler);
 
 server.listen(process.env.PORT || 3000, () => {
-  console.log(`Rodando na porta: ${process.env.PORT}`);
+  console.log(`Porta: ${process.env.PORT}`);
 });
